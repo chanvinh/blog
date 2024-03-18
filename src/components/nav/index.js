825e971473320db55aec868550998996c875b48a
KFN TAHuy <fj5553fe@aa.jp.fujitsu.com>
2024-03-18 10:04:40 +0700

    fix: update code

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,61 +1,55 @@
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
-import React, { useCallback, useState, useReducer } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import home from "../../images/home-phones.png";
-import { reducerNav } from "../../redux/nav/reducer";
 import "./nav.scss";
 import { api, list, list__key } from "./list";
 
 const Nav = () => {
-  const [state, dispatch] = useReducer(reducerNav, { id: "1", key: "" });
-  const [id, setId] = useState(state.id);
-  const [key, setKey] = useState(state.key);
+  const [id, setId] = useState(
+    localStorage.getItem("id")
+      ? String(JSON.parse(localStorage.getItem("id")))
+      : "1"
+  );
+  const [key, setKey] = useState(
+    localStorage.getItem("key")
+      ? String(JSON.parse(localStorage.getItem("key")))
+      : ""
+  );
   const [search, setSearch] = useState("");
-  const [showSearch, setShowSearch] = useState(false);
+  const [showSearch, setShowSearch] = useState(
+    localStorage.getItem("id")
+      ? Boolean(JSON.parse(localStorage.getItem("showSearch")))
+      : false
+  );
 
   const handleClick = useCallback((id, key) => {
-    if (id === "0") {
+    if (id === "0" || id === "1") {
       setId("1");
       setKey(key ? key : "");
-      dispatch({
-        type: "CLICK",
-        payload: {
-          id,
-          key,
-        },
-      });
       setShowSearch(false);
+      window.localStorage.clear();
       return;
     }
 
     if (key === "2") {
       setId(id);
       setKey(key ? key : "");
-      dispatch({
-        type: "CLICK",
-        payload: {
-          id,
-          key,
-        },
-      });
       setShowSearch(true);
       return;
     }
 
     setId(id);
     setKey(key ? key : "");
-    dispatch({
-      type: "CLICK",
-      payload: {
-        id,
-        key,
-      },
-    });
     setShowSearch(false);
   }, []);
 
-console.log(state)
+  useEffect(() => {
+    localStorage.setItem("id", id);
+    localStorage.setItem("key", key);
+    localStorage.setItem("showSearch", showSearch);
+  }, [id, key, showSearch]);
 
   return (
     <>
@@ -96,9 +90,7 @@ console.log(state)
                   <li
                     onClick={() => handleClick(item.id, item.key)}
                     className={
-                      item.id === state.id && item.key === state.key
-                        ? "active_nav_key"
-                        : ""
+                      item.id === id && item.key === key ? "active_nav_key" : ""
                     }
                   >
                     {item.id && !item.key ? (
