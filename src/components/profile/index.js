30eafa0fa6fdddab50eec2fcd70dff2976fe85dc
KFN TAHuy <fj5553fe@aa.jp.fujitsu.com>
2024-03-19 11:37:13 +0700

    fix: update code

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,10 +1,99 @@
-import React from 'react'
-import './profile.scss'
+import React, { useEffect, useState } from "react";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ModeCommentOutlinedIcon from "@mui/icons-material/ModeCommentOutlined";
+import "./profile.scss";
+import banner from "../../images/banner.png";
+import home from "../../images/home-phones.png";
+import { list } from "./list";
 
 const Profile = () => {
-  return (
-    <div className='profile'>Profile</div>
-  )
-}
+  const [id, setId] = useState(
+    localStorage.getItem("id_profile")
+      ? String(localStorage.getItem("id_profile"))
+      : "1"
+  );
 
-export default Profile
+  useEffect(() => {
+    localStorage.setItem("id_profile", id);
+  }, [id]);
+
+  return (
+    <div className="profile">
+      <div className="profile_top">
+        <div className="profile_top_left">
+          <img src={banner} />
+        </div>
+        <div className="profile_top_right">
+          <div className="name_setting">
+            <h2>chanvinh0411</h2>
+            <div className="setting">
+              <button>Edit profile</button>
+              <span>
+                <SettingsOutlinedIcon></SettingsOutlinedIcon>
+              </span>
+            </div>
+          </div>
+          <div className="list">
+            <ul>
+              <li>
+                <span>2</span> posts
+              </li>
+              <li>
+                <span>2</span> followers
+              </li>
+              <li>
+                <span>2</span> following
+              </li>
+            </ul>
+          </div>
+          <div className="bio">
+            <p>Chấn Vinh</p>
+            <p>Chấn Vinh (陈荣)</p>
+          </div>
+        </div>
+      </div>
+      <div className="profile_footer">
+        <div className="profile_footer_top">
+          <ul>
+            {list.map((item) => {
+              return (
+                <li
+                  onClick={() => setId(item.id)}
+                  key={item.id}
+                  className={item.id === id ? "active_list" : ""}
+                >
+                  <span>{item.icon}</span>
+                  {item.name}
+                </li>
+              );
+            })}
+          </ul>
+          {id === "1" && (
+            <div className="profile_post">
+              <div className="item_post">
+                <img src={banner} />
+                <div className="hover_item">
+                  <p>
+                    <span>
+                      <FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon>
+                    </span>
+                    28
+                  </p>
+                  <p>
+                    <span>
+                      <ModeCommentOutlinedIcon></ModeCommentOutlinedIcon>
+                    </span>
+                    28
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/components/profile/list.js
@@ -0,0 +1,20 @@
+import StickyNote2OutlinedIcon from '@mui/icons-material/StickyNote2Outlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
+export const list = [
+  {
+    id: '1',
+    icon: <StickyNote2OutlinedIcon></StickyNote2OutlinedIcon>,
+    name: 'POSTS'
+  },
+  {
+    id: '2',
+    icon: <BookmarkBorderOutlinedIcon></BookmarkBorderOutlinedIcon>,
+    name: 'SAVED'
+  },
+  {
+    id: '3',
+    icon: <AccountBoxOutlinedIcon></AccountBoxOutlinedIcon>,
+    name: 'TAGGED'
+  },
+]
